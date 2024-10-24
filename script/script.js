4bc3da2f6a92c27c94509f7d6b234f95a6caeb5a
Ragum02 <benjamin.souiket@gmail.com>
2024-10-24 17:41:09 +0200

    Project complete

--- script/script.js
+++ script/script.js
@@ -1,1 +1,18 @@
-console.log("Hello, World!")
+const password = document.querySelector("#password");
+const confirmPassword = document.querySelector("#confirm_password");
+
+const errorPassword = document.querySelector(".error_password");
+
+function checkPasswords(){
+    if(confirmPassword.value === ""){
+        return;
+    }
+else if (password.value !== confirmPassword.value) {
+    errorPassword.classList.add("show");
+} else {
+    errorPassword.classList.remove("show");
+}
+}
+
+password.addEventListener('input', checkPasswords);
+confirmPassword.addEventListener('input', checkPasswords);
